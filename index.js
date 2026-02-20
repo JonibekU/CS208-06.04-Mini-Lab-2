c95a0d9c5fed3bff40caeef1f297cb199a8d6bbf
JonibekU <Jonibekutaev@u.boisestate.edu>
2026-02-20 04:56:17 +0000

    finished

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 function printSum(x, y) {
+  const numX = parseFloat(x);
+  const numY = parseFloat(y);
 
+  const xIsNaN = isNaN(numX);
+  const yIsNaN = isNaN(numY);
+
+  if (!xIsNaN && !yIsNaN) {
+    console.log(`Sum is ${numX + numY}.`);
+  } 
+  else if (xIsNaN && yIsNaN) {
+    console.log(`'${x}' and '${y}' are not numbers.`);
+  } 
+  else if (xIsNaN) {
+    console.log(`'${x}' is not a number.`);
+  } 
+  else {
+    console.log(`'${y}' is not a number.`);
+  }
 
 }
 
